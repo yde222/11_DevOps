9d541f5285e1f9186feb6c1898b4ea3a4ebb9634
macacolabs <monkey.ohgiraffers@gmail.com>
2025-06-20 15:31:47 +0900

    feat: docker compose

--- 00_YUSEUNGJE/01_docker/docker-compose/chap01-02-react-project/next.config.mjs
+++ 00_YUSEUNGJE/01_docker/docker-compose/chap01-02-react-project/next.config.mjs
@@ -5,8 +5,9 @@ const nextConfig = {
       {
         source: "/boot/:path*",
         // destination: "http://localhost:8080/:path*",
-        destination: "http://host.docker.internal:8080/:path*",
+        // destination: "http://host.docker.internal:8080/:path*",
         // 도커 내부에서는 localhost가 React컨테이너 자신을 의미
+        destination: "http://backend:8080/:path*",
       },
     ];
   },
