54ea0a3b76f98e0c26b1136137a67f050a882112
macacolabs <monkey.ohgiraffers@gmail.com>
2025-06-20 12:41:57 +0900

    fix: source

--- 00_YUSEUNGJE/01_docker/docker-compose/chap01-02-react-project/next.config.mjs
+++ 00_YUSEUNGJE/01_docker/docker-compose/chap01-02-react-project/next.config.mjs
@@ -4,7 +4,9 @@ const nextConfig = {
     return [
       {
         source: "/boot/:path*",
-        destination: "http://localhost:8080/:path*",
+        // destination: "http://localhost:8080/:path*",
+        destination: "http://host.docker.internal:8080/:path*",
+        // 도커 내부에서는 localhost가 React컨테이너 자신을 의미
       },
     ];
   },
