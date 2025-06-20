ed56c81eebf73502c194062b68738c3693643812
macacolabs <monkey.ohgiraffers@gmail.com>
2025-06-20 10:51:42 +0900

    first commit

---
+++ 00_YUSEUNGJE/01_docker/docker-compose/chap01-01-boot-project/src/main/java/com/ohgiraffers/bootproject/config/WebConfig.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.bootproject.config;
+
+public class WebConfig {
+}
---
+++ 00_YUSEUNGJE/01_docker/dockerImage/src/test/java/com/ohgiraffers/dockerimage/DockerImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.dockerimage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerImageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
