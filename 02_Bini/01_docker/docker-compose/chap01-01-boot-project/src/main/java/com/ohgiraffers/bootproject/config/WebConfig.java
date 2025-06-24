d47b00e9aa98f16c3dfbc7a115a1d00f6cea53bb
binipk <mingyoku93@gmail.com>
2025-06-24 14:05:08 +0900

    update

--- 02_Bini/01_docker/docker-compose/chap01-01-boot-project/src/main/java/com/ohgiraffers/bootproject/config/WebConfig.java
+++ 02_Bini/01_docker/docker-compose/chap01-01-boot-project/src/main/java/com/ohgiraffers/bootproject/config/WebConfig.java
@@ -11,8 +11,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 // next.js에 설정된 3000번 포트에서오는 건 cors설정 해제
-                .allowedOrigins("http://localhost:3000")
+//                .allowedOrigins("http://localhost:3000")
+                // Nodeport에 설정된 30000번 포트에서오는 연결에 대한 cors설정 해제
+                .allowedOrigins("http://localhost:30000")
                 .allowedOrigins("") // Ingress 설정으로 내부적으로 통신이 수행되므로 CORS 설정 없이도 통신가능
                 .allowedMethods("GET", "POST", "DELETE", "OPTIONS", "FETCH");
+        
     }
 }
