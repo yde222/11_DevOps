fbe6223b3cea2125086507fb6d3de0db8d237954
jungho <kiik52@naver.com>
2025-06-24 12:46:06 +0900

    // NodePort에 설정된 30000번 포트에서 오는 연결에 대한 CQRS 설정 해제                 .allowedOrigins("http://localhost:30000")

--- 20_JIJUNGHO/01_docker/docker-compose/chap01-01-boot-project/src/main/java/com/ohgiraffers/boot/config/WebConfig.java
+++ 20_JIJUNGHO/01_docker/docker-compose/chap01-01-boot-project/src/main/java/com/ohgiraffers/boot/config/WebConfig.java
@@ -13,7 +13,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 // next.js에 설정된 3000번 포트에서 오는건 cors 설정 해제
-                .allowedOrigins("http://localhost:3000")
+//                .allowedOrigins("http://localhost:3000")
+                // NodePort에 설정된 30000번 포트에서 오는 연결에 대한 CQRS 설정 해제
+                .allowedOrigins("http://localhost:30000")
                 .allowedOrigins("") // Ingress설정으로 내부적으로 통신이 수행이되므로 CORS 설정없어도 통신 가능
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "FETCH");
 
