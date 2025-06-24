621b8c804765f7bf1fbb550678ecf3a15b90ca5b
nyaeon <nyaooe@gmail.com>
2025-06-24 16:19:29 +0900

    25-06-24 실습

---
+++ 16_LIMNAYEON/01_docker/docker-compose/chap01-01-boot-project/src/main/java/com/ohgiraffers/bootproject/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.ohgiraffers.bootproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                // next.js에 설정된 3000번 포트에서오는 건 cors설정 해제
+                .allowedOrigins("http://localhost:30000")
+                .allowedOrigins("") // Ingress설정으로 내부적으로 통신이 수행이되므로 CORS설정 없어도 통신가능
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","FETCH");
+    }
+}
