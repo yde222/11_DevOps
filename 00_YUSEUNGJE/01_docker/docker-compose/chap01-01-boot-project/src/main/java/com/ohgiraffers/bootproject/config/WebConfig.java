8b910842b863981b6e0124a141d7d7dc825f488d
macacolabs <monkey.ohgiraffers@gmail.com>
2025-06-25 12:15:05 +0900

    feat: ingress upload

--- 00_YUSEUNGJE/01_docker/docker-compose/chap01-01-boot-project/src/main/java/com/ohgiraffers/bootproject/config/WebConfig.java
+++ 00_YUSEUNGJE/01_docker/docker-compose/chap01-01-boot-project/src/main/java/com/ohgiraffers/bootproject/config/WebConfig.java
@@ -13,8 +13,8 @@ public class WebConfig implements WebMvcConfigurer {
                 // next.js에 설정된 3000번 포트에서오는 건 cors설정 해제
 //                .allowedOrigins("http://localhost:3000")
                 // NodePort에 설정된 30000번 포트에서 오는 연결에 대한 CQRS설정 해제
-                .allowedOrigins("http://localhost:30000")
-                .allowedOrigins("") // Ingress설정으로 내부적으로 통신이 수행이되므로 CORS설정 없어도 통신가능
+//                .allowedOrigins("http://localhost:30000")
+                //.allowedOrigins("") // Ingress설정으로 내부적으로 통신이 수행이되므로 CORS설정 없어도 통신가능
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","FETCH");
     }
 }
